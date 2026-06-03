18b6fa2764a58f5ac436c9184d01a26f485bc0a4
BraxtonW10 <161777065+BraxtonW10@users.noreply.github.com>
2026-06-03 02:32:31 -0500

    Create world.js

---
+++ world.js
@@ -0,0 +1,55 @@
+const WORLD = {
+
+towns:[
+
+{
+id:"pallet",
+name:"Pallet Town",
+x:5,
+y:5,
+gym:false
+},
+
+{
+id:"forest",
+name:"Forest City",
+x:25,
+y:10,
+gym:1
+},
+
+{
+id:"port",
+name:"Port City",
+x:45,
+y:20,
+gym:2
+}
+
+],
+
+routes:[
+
+{
+name:"Route 1",
+x1:5,
+y1:5,
+x2:25,
+y2:10,
+levelMin:2,
+levelMax:6
+},
+
+{
+name:"Route 2",
+x1:25,
+y1:10,
+x2:45,
+y2:20,
+levelMin:6,
+levelMax:12
+}
+
+]
+
+};
