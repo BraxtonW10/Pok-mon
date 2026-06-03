8919c85f6df457b3b2209dad052e9b8f4e01d1ea
BraxtonW10 <161777065+BraxtonW10@users.noreply.github.com>
2026-06-03 02:49:32 -0500

    Update world.js

--- world.js
+++ world.js
@@ -6,24 +6,86 @@ towns:[
 id:"pallet",
 name:"Pallet Town",
 x:5,
-y:5,
+y:5
+},
+
+{
+id:"viridian",
+name:"Viridian City",
+x:15,
+y:10,
 gym:false
 },
 
 {
-id:"forest",
-name:"Forest City",
+id:"pewter",
+name:"Pewter City",
 x:25,
 y:10,
 gym:1
 },
 
 {
-id:"port",
-name:"Port City",
-x:45,
-y:20,
+id:"cerulean",
+name:"Cerulean City",
+x:40,
+y:8,
 gym:2
+},
+
+{
+id:"vermilion",
+name:"Vermilion City",
+x:55,
+y:18,
+gym:3
+},
+
+{
+id:"lavender",
+name:"Lavender Town",
+x:70,
+y:15
+},
+
+{
+id:"celadon",
+name:"Celadon City",
+x:80,
+y:10,
+gym:4
+},
+
+{
+id:"fuchsia",
+name:"Fuchsia City",
+x:90,
+y:25,
+gym:5
+},
+
+{
+id:"saffron",
+name:"Saffron City",
+x:100,
+y:15,
+gym:6
+},
+
+{
+id:"cinnabar",
+name:"Cinnabar Island",
+x:120,
+y:35,
+gym:7
+},
+
+{
+id:"viridiangym",
+name:"Viridian Gym",
+x:130,
+y:8,
+gym:8
 }
 
 ],
@@ -32,24 +94,130 @@ routes:[
 
 {
 name:"Route 1",
-x1:5,
-y1:5,
-x2:25,
-y2:10,
 levelMin:2,
-levelMax:6
+levelMax:5
 },
 
 {
 name:"Route 2",
-x1:25,
-y1:10,
-x2:45,
-y2:20,
-levelMin:6,
+levelMin:4,
+levelMax:8
+},
+
+{
+name:"Route 3",
+levelMin:8,
 levelMax:12
+},
+
+{
+name:"Route 4",
+levelMin:12,
+levelMax:16
+},
+
+{
+name:"Route 5",
+levelMin:15,
+levelMax:20
+},
+
+{
+name:"Route 6",
+levelMin:20,
+levelMax:25
+},
+
+{
+name:"Route 7",
+levelMin:25,
+levelMax:30
+},
+
+{
+name:"Route 8",
+levelMin:30,
+levelMax:35
+},
+
+{
+name:"Route 9",
+levelMin:35,
+levelMax:40
+},
+
+{
+name:"Route 10",
+levelMin:40,
+levelMax:50
 }
 
-]
+],
+
+caves:[
+
+{
+name:"Viridian Forest",
+x:18,
+y:6
+},
+
+{
+name:"Mt Moon",
+x:32,
+y:7
+},
+
+{
+name:"Rock Tunnel",
+x:72,
+y:10
+},
+
+{
+name:"Seafoam Islands",
+x:115,
+y:30
+},
+
+{
+name:"Victory Road",
+x:140,
+y:5
+}
+
+],
+
+legendaryLocations:[
+
+{
+name:"Power Plant",
+pokemon:"Zapdos"
+},
+
+{
+name:"Seafoam Islands",
+pokemon:"Articuno"
+},
+
+{
+name:"Volcano Chamber",
+pokemon:"Moltres"
+},
+
+{
+name:"Unknown Cave",
+pokemon:"Mewtwo"
+}
+
+],
+
+league:{
+
+name:"Pokemon League",
+x:150,
+y:5
+
+}
 
 };
